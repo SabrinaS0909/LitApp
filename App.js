a7e14572186f4612a3305086701a69960e686445
Sabrina Spruck <113223660+SabrinaS0909@users.noreply.github.com>
2023-03-30 00:01:00 -0400

    combined css files

--- App.js
+++ App.js
@@ -4,6 +4,38 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Front/>
+      <Inside/>
+      <Back/>
+    </div>
+  );
+}
+
+function Front() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+function Inside() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+function Back() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+function Loading() {
+  return (
+    <div>
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,5 +53,38 @@ function App() {
     </div>
   );
 }
+
+  
+      <body>
+        <img src="%PUBLIC_URL%/frontOfBook.png" alt="The Front of Your Book" width="1334px" height="750px"></img> 
+        <form action="{file that the form data should be sent to}" method="post">
+          <div>
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+            <button type="submit">Login</button>
+            <label>
+              <input type="checkbox" checked="checked" name="remember"></input> 
+              <p>Remember me</p>
+            </label>
+          </div>
+
+          <div>
+            <button id="cancel">Cancel</button>
+            <span class="psw"><a href="#">Forgot password?</a></span>
+            <p>Not a member yet? <a href="#">Sign up!!</a></p>
+          </div>
+        </form>
+      </body>
+    </div>
+  );
+}
+
+
+	
+  
 
 export default App;
