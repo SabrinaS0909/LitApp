c7cd8098f407f8bd2245b9a11b7564f9e28e9586
Sabrina Spruck <113223660+SabrinaS0909@users.noreply.github.com>
2023-04-27 04:09:27 -0400

    Add files via upload

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
     'https://www.fancyspiders.com',
+    'https://fancyspiders.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000'
-];
+    'http://localhost:3000',
+]; //Take out all except fancyspiders.com after development
 
 module.exports = allowedOrigins;
